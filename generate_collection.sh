7f23c9f7963317a249d27bd2936d15a00bbfade2
Jakub Dolejs <jakubdolejs@gmail.com>
2025-09-24 20:37:08 +0100

    Added script that generates and signs a collection

---
+++ generate_collection.sh
@@ -0,0 +1,3 @@
+package-collection-generate packages.json collection_unsigned.json
+package-collection-sign collection_unsigned.json collection.json key.pem swift_package.der AppleWWDRCAG3.der AppleIncRootCertificate.der
+rm collection_unsigned.json
